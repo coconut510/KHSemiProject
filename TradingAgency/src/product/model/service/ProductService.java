d91726facc98f9cea98956c8e06ab178143195cf
kyh <coconut76002027@gmail.com>
2018-06-15 19:53:33 +0900

    상품검색시

--- TradingAgency/src/product/model/service/ProductService.java
+++ TradingAgency/src/product/model/service/ProductService.java
@@ -1,5 +1,17 @@
 package product.model.service;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import common.JDBCTemplate;
+import product.model.vo.Product;
+
 public class ProductService {
+
+	public ArrayList<Product> ProductSearchKeyword(String keyword) {
+		Connection conn = JDBCTemplate.getConnection();
+		System.out.println("키워드 " + keyword);
+		return null;
+	}
 
 }
--- TradingAgency/src/product/model/vo/Product.java
+++ TradingAgency/src/product/model/vo/Product.java
@@ -1,5 +1,62 @@
 package product.model.vo;
 
 public class Product {
-
+	private int product_entire_pk;
+	private String product_entire_user_entire_id_fk;
+	private String product_entire_category_main_id_fk;
+	private String product_entire_category_sub_id_fk;
+	private String product_name;
+	private int product_price;
+	private int product_amount;
+	private String product_state;
+	public int getProduct_entire_pk() {
+		return product_entire_pk;
+	}
+	public void setProduct_entire_pk(int product_entire_pk) {
+		this.product_entire_pk = product_entire_pk;
+	}
+	public String getProduct_entire_user_entire_id_fk() {
+		return product_entire_user_entire_id_fk;
+	}
+	public void setProduct_entire_user_entire_id_fk(String product_entire_user_entire_id_fk) {
+		this.product_entire_user_entire_id_fk = product_entire_user_entire_id_fk;
+	}
+	public String getProduct_entire_category_main_id_fk() {
+		return product_entire_category_main_id_fk;
+	}
+	public void setProduct_entire_category_main_id_fk(String product_entire_category_main_id_fk) {
+		this.product_entire_category_main_id_fk = product_entire_category_main_id_fk;
+	}
+	public String getProduct_entire_category_sub_id_fk() {
+		return product_entire_category_sub_id_fk;
+	}
+	public void setProduct_entire_category_sub_id_fk(String product_entire_category_sub_id_fk) {
+		this.product_entire_category_sub_id_fk = product_entire_category_sub_id_fk;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public int getProduct_price() {
+		return product_price;
+	}
+	public void setProduct_price(int product_price) {
+		this.product_price = product_price;
+	}
+	public int getProduct_amount() {
+		return product_amount;
+	}
+	public void setProduct_amount(int product_amount) {
+		this.product_amount = product_amount;
+	}
+	public String getProduct_state() {
+		return product_state;
+	}
+	public void setProduct_state(String product_state) {
+		this.product_state = product_state;
+	}
+	
+	
 }
