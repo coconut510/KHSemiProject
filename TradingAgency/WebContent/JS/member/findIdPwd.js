4f44ec4bc6e484375d5dbbf7159dca7f9a100783
BAKKI\bakki <gpdbs532@naver.com>
2018-06-16 11:36:54 +0900

    Revert "g"
    This reverts commit b21b39b6e2d3c96d63113158c354904da71c1f96.

--- TradingAgency/WebContent/JS/member/findIdPwd.js
+++ TradingAgency/WebContent/JS/member/findIdPwd.js
@@ -8,12 +8,12 @@ function findIDBtn() {
 }
 
 $(document).ready(function(){
-	$("input[name='idCertificationNum']").prop("disabled",true);
+	$("input[name='IDcertificationNum']").prop("disabled",true);
 });
 
 
-function IdCertificationBtn() {
-			$("input[name='idCertificationNum']").prop("disabled", false);
+function IdcertificationBtn() {
+			$("input[name='IDcertificationNum']").prop("disabled", false);
 			console.log("g");
 
 }
@@ -30,37 +30,3 @@ function PwdcertificationNum() {
 
 }
 	
-function findIDBtn(){
-	var memberId = $("#memberName_id").val();
-	var memberEmail = $("#memberEmail_id").val();
-	var idCertificationNum = $("#idCertificationNum").val();
-	console.log(memberId);
-	if(!memberId&&!memberEmail&&!idCertificationNum){
-		alert("비정상");
-		location.href="../../views/member/findIdPwd.jsp";
-	}else{
-		alert("정상");
-		location.href="../../views/member/findIdComplete.jsp";
-	
-	}
-}
-
-
-function resetPwd(){
-	var memberName= $("#memberName_pwd").val();
-	var memberId = $("#memberName_pwd").val();
-	var memberEmail = $("#memberEmail_pwd").val();
-	var pwdCertificationNum = $("#pwdCertificationNum").val();
-
-	if(!memberName&&!memberId&&!memberEmail&&!pwdCertificationNum){
-		alert("비밀번호 변경불가능");
-		location.href="../../views/member/findIdPwd.jsp";
-	}else{
-		alert("비밀번호 변경가능");
-		location.href="../../views/member/findIdComplete.jsp";
-	
-		
-		
-	
-	}
-}
